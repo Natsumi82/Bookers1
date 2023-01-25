69107a4cae4e09f9a371e6aa024ffbf21c35d87c
Natsumi82 <122456886+Natsumi82@users.noreply.github.com>
2023-01-25 15:15:23 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,44 @@
+class BooksController < ApplicationController
+  def new
+    @book =Book.new
+  end
+
+  def create
+     @book= Book.new(list_params)
+
+     if @book.save
+      flash[:complete] = “Successfully!”
+     else
+
+     end
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+     redirect_to books_path, alert: 'Destroy'
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
