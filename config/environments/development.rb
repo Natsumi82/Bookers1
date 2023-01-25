9d4592716b4173fb53b15d37369862b2cb89f50b
Natsumi82 <122456886+Natsumi82@users.noreply.github.com>
2023-01-25 16:03:56 +0000

    20230125

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "39a480b8b1594449bf8bf919f9c2cc75.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts.clear
+
 end
