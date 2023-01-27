b32b1ab8ee2590de5b2c1f1e59a8c6236a473798
Natsumi82 <122456886+Natsumi82@users.noreply.github.com>
2023-01-27 14:24:36 +0000

    [Add] kadai2-11

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,17 @@ class BooksController < ApplicationController
      @book= Book.new(book_params)
 
      if @book.save
-      flash[:complete] = “Successfully!”
+        redirect_to book_path(@book.id)
      else
-
+      @books = Book.all
+      render :index
      end
+    
+    book = Book.new(book_params)
+    book.save
+     flash[:notice] = "Successfly!"
+     redirect_to book_path(book.id)
+    
   end
 
   def index
