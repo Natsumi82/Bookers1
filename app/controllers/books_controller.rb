c75449863cb75fdb81ecc6f0fd40e0d6fa6bfbee
Natsumi82 <122456886+Natsumi82@users.noreply.github.com>
2023-01-30 10:03:36 +0000

    [Add] croud

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,24 +4,14 @@ class BooksController < ApplicationController
   end
 
   def create
-     @book= Book.new(book_params)
-
-     if @book.save
-        redirect_to book_path(@book.id)
-     else
-      @books = Book.all
-      render :index
-     end
-    
-    book = Book.new(book_params)
-    book.save
-     flash[:notice] = "Successfly!"
-     redirect_to book_path(book.id)
-    
+     book = Book.new(book_params)
+     book.save
+      redirect_to books_path(book.id)
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
